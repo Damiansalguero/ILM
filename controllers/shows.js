012384dab20809bfcd0fd4e436c22118ded151a2
Damiansalguero <damian.salguero@aikq.de>
2021-04-02 12:12:37 +0200

    add landing page + restructure main page + add assets

--- controllers/shows.js
+++ controllers/shows.js
@@ -2,3 +2,8 @@ module.exports.renderLanding = (req, res) => {
   res.render("landing");
   // res.render("test");
 };
+
+module.exports.rendermain = (req, res) => {
+  res.render("main");
+  // res.render("test");
+};
--- routes/shows.js
+++ routes/shows.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const shows = require("../controllers/shows");
 const catchAsync = require("../utils/catchAsync");
-module.exports = router;
 
 router.get("/", shows.renderLanding);
+router.get("/home", shows.rendermain);
+
+module.exports = router;
